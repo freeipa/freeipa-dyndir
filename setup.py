886eb01e87fa8decf821e6b560a6b57f4376644e
Milan Kubík <mkubik@redhat.com>
2017-05-19 16:22:08 +0200

    Increase the version to 0.2
    An backward incompatible change was introduced by removing the package
    names metadata from the inventory core.
    
    If necessary, it is hereby advised to provide the metadata in other ways
    supported by ansible, such as variables defined on role or playbook
    level.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='ipaqe-dyndir',
-    version='0.1.4',
+    version='0.2.0',
     description='Ansible dynamic inventory for FreeIPA',
     long_description=long_description,
     keywords='freeipa tests ansible',
