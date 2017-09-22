f617ccfa5ba9e33941b6493c38b4b338ffa59af9
Milan Kubík <mkubik@redhat.com>
2017-09-22 13:28:24 +0200

    Bugfix release update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='ipaqe-dyndir',
-    version='0.2.0',
+    version='0.2.1',
     description='Ansible dynamic inventory for FreeIPA',
     long_description=long_description,
     keywords='freeipa tests ansible',
